5089a6f6c6d4ae5f9a2367f64e8539610660f7c1
Dario Romano <d.salvo.romano@gmail.com>
2021-03-15 11:46:36 +0100

    Initial commit

---
+++ test.module1/src/main/java/test/test/parent/App.java
@@ -0,0 +1,16 @@
+package test.test.parent;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String s=IOUtils.LINE_SEPARATOR;
+    }
+}
